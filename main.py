c327fa8d34990e055d573359cbcad8240324eb95
Sebastian Lee <96149632+sebiprograms@users.noreply.github.com>
2025-06-03 17:56:01 -0500

    Updated csv data in useful format
    Csv practice data is reformated in a 2D array form
    that is expected of information in a linear regression
    model. In this case we have only one dependent variable
    so there is only one column.

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.axes as ax
+from matplotlib.animation import FuncAnimation
+
+
+url = './data_for_lr.csv' 
+data = pd.read_csv(url)
+
+data = data.dropna()
+
+train_input = np.array(data.x[0:500]).reshape(500,1)
+train_output = np.array(data.x[0:500]).reshape(500,1)
+
+test_input = np.array(data.x[500:700]).reshape(199,1)
+test_output = np.array(data.y[500:700]).reshape(199,1)
